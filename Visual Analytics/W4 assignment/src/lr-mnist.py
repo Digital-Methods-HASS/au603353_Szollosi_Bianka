46229c5ba7c93bd7694e619033e4fdc8061ab33a
szbianka <54929459+szbianka@users.noreply.github.com>
2021-03-24 01:11:55 +0100

    Add files via upload

--- Visual Analytics/W4 assignment/src/lr-mnist.py
+++ Visual Analytics/W4 assignment/src/lr-mnist.py
@@ -15,9 +15,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+import argparse
 
 #defining main() function
 def main():
+    #argument parser
+    ap = argparse.ArgumentParser()
+    
+    #command line arguments to define
+    ap.add_argument("-o", "--outfile", required=True, help="Output filename")
+    
+    #parse arguments
+    args = vars(ap.parse_args())
+    
+    #creating variables that can be defined in command line
+    out_file_name = args["outfile"]
+                    
+    #creating a folder called out, if it doesn't exist (to save the output files here)
+    out = os.path.join("..", "out")
+    if not os.path.exists(out):
+        os.mkdir(out)
     
     #fetching data, where X is the dataset and y is labels of the data
     X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
@@ -37,8 +54,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, #our data
                                                         y, #labels
                                                         random_state=9, #makes it reproducible
-                                                        train_size=0.8, #splitting by 80%-20%
-                                                        test_size=0.2)
+                                                        test_size= 0.2)
 
     #scaling the features by deviding by 255, so the scale features are between 0-1
     X_train_scaled = X_train/255.0
@@ -60,7 +76,7 @@ def main():
     print(confusion_matrix)
 
     #saving the output as a txt file
-    outpath = os.path.join("..", "out", "lr_confusion_matrix.txt")
+    outpath = os.path.join("..", "out", out_file_name)
     file = open(outpath, "w")
     file.write(confusion_matrix)
     file.close()
